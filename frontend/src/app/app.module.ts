07609ccd59ae8c60f378162d14108b3cd6055955
vinicius-tx <dev.viniciusmello@gmail.com>
2020-07-23 17:26:53 -0300

    implementando directives de atributo e de estrutura

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,13 +9,26 @@ import { FooterComponent } from './components/templates/footer/footer.component'
 import { NavComponent } from './components/templates/nav/nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card'
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ResDirective } from './directives/res.directive';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+
+// DENTRO DE DECLARATIONS, COLOCAMOS OS COMPONENTES, DIRETIVAS E OS PIPES
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    ResDirective,
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
@@ -23,7 +36,8 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/directives/res.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appRes]'
+})
+export class ResDirective {
+
+  constructor() { }
+
+}
